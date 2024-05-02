3b6bca42574076450d182cfc743679437d825c0a
MatteoGiunta97 <matteogiunta@outlook.it>
2024-05-02 19:20:07 +0200

    feat: add bootstrap

--- index.php
+++ index.php
@@ -58,14 +58,15 @@ Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gl
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Hotels</title>
 </head>
 <body>
     <h1>Hotels:</h1>
-    
-    <ul>
+
+    <ul class="list-group list-group-horizontal">
         <?php foreach($hotels as $hotel) { ?>
-            <li>
+            <li class="list-group-item">
                 <div>Nome:<?php echo $hotel['name']; ?></div>
                 <div>Descrizione:<?php echo $hotel['description']; ?></div>
                 <div>Parcheggio:<?php echo $hotel['parking'] ?></div>
@@ -74,5 +75,7 @@ Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gl
             </li>
         <?php } ?>
     </ul>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
